14bc0a6c800e26a2a6d64d58f320980a1d530fc6
Chris Free <cfree14@gmail.com>
2021-06-03 13:20:45 -0700

    tried to fix calc SEV

--- R/calc_sev.R
+++ R/calc_sev.R
@@ -3,22 +3,21 @@
 #' This function calculates the summary exposure value (SEV), or percent deficiency, for a group based on its Estimated Average Requirement and its habitual intake distribution.
 #'
 #' @param ear Estimated Average Requirement (EAR)
+#' @param cv Coefficient of variation (CV)
 #' @param shape Shape parameter for gamma distribution
 #' @param rate Rate parameter for gamma distribution
 #' @param meanlog Mean parameter for gamma distribution
 #' @param sdlog Standard deviation parameter for gamma distribution
 #' @param plot Plot the distributions relative to the EAR? True/False
 #' @return The percent of a population deficient in a nutrient (SEV)
-#' @examples
-#' dist_do <- get_dists(nutrients="Calcium", isos="USA", sexes="F", ages=25)
-#' ear <- dris %>% filter(nutrient=="Calcium" & sex=="Females" & age_range=="19-30 yr" & stage=="None" & dri_type=="Estimated Average Requirement (EAR)")
+#' @export
 calc_sev <- function(ear, cv, shape=NULL, rate=NULL, meanlog=NULL, sdlog=NULL, plot=F){
 
   # For testing
-  cv <-  0.1 # 0.25 for Vitamin B-12 and 0.10 for everything else
-  ear <- ear$value
-  shape <- dist_do$g_shape
-  rate <- dist_do$g_rate
+  # cv <-  0.1 # 0.25 for Vitamin B-12 and 0.10 for everything else
+  # ear <- ear$value
+  # shape <- dist_do$g_shape
+  # rate <- dist_do$g_rate
 
   # Define habitual intake distribution
   Intake <- function(x){dgamma(x, shape=shape, rate=rate)}
